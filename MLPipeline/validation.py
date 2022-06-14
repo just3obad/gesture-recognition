c2785251a6ac7f9dcce66e4f2a9f18ace27f357d
just3obad <just3obad@gmail.com>
2022-06-14 14:53:55 +0200

    fix figure

--- MLPipeline/validation.py
+++ MLPipeline/validation.py
@@ -5,10 +5,13 @@ import pandas as pd
 def plot_loss(loss_csv, save_fig=False):
     loss_df = pd.read_csv(loss_csv)
     print(loss_df["loss"])
-    plt.plot(loss_df["loss"])
+    fig = plt.figure(figsize=(6, 6))
+    plt.plot(loss_df["loss"], label="loss")
     plt.show()
+    if save_fig:
+        fig.savefig("loss.png")
 
 
 if __name__ == '__main__':
     lr_loss_path = "models/loss.csv"
-    plot_loss(lr_loss_path)
+    plot_loss(lr_loss_path, save_fig=True)
